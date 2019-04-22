e1ddfc4fbecbfd481bf674c0e2e71e5c2a256af2
Fan Zhang <vanadiumzhang@gmail.com>
2019-04-21 22:06:31 -0500

    CHANGE: knapsack - ADD: return the set of items

---
+++ dynamic_programming/knapsack/knapsack.py
@@ -0,0 +1,38 @@
+from typing import List, Tuple
+
+
+class Item(object):
+    def __init__(self, value: int, weight: int):
+        self.value = value
+        self.weight = weight
+
+
+def knapsack(max_weight: int, items: List[Item])->Tuple[int, set]:
+    """
+    :param max_weight: the capacity of knapsack (W > 0)
+    :param items: n items, each item has a weight > 0 and a value >0
+    :return: a set of items, such as the total weights <= W, and the total value is maximized
+    """
+    # a matrix opt[items size(0,...,n)[weight(0, ..., total_weight]]
+    opt = [[-1 for _ in range(max_weight + 1)] for _ in range(len(items) + 1)]
+    item_sets = [[set() for _ in range(max_weight + 1)] for _ in range(len(items) + 1)]
+
+    for w in range(max_weight + 1):
+        opt[0][w] = 0
+
+    for i in range(1, len(items) + 1):
+        for w in range(max_weight + 1):
+            if items[i - 1].weight > w:
+                opt[i][w] = opt[i - 1][w]
+                item_sets[i][w] = item_sets[i - 1][w]
+            else:
+                if opt[i - 1][w] >= items[i - 1].value + opt[i - 1][w - items[i - 1].weight]:
+                    opt[i][w] = opt[i - 1][w]
+                    item_sets[i][w] = item_sets[i - 1][w]
+                else:
+                    opt[i][w] = items[i - 1].value + opt[i - 1][w - items[i - 1].weight]
+                    item_set = item_sets[i - 1][w - items[i - 1].weight].copy()
+                    item_set.add(items[i - 1].value)
+                    item_sets[i][w] = item_set
+    return opt[-1][-1], item_sets[-1][-1]
+
--- dynamic_programming/knapsack/test_knapsack.py
+++ dynamic_programming/knapsack/test_knapsack.py
@@ -5,5 +5,5 @@ from knapsack import knapsack, Item
 class TestKnapsack(TestCase):
     def test_knapsack(self):
         items = [Item(1, 1), Item(6, 2), Item(18, 5), Item(22, 6), Item(28, 7)]
-        self.assertEqual(40, knapsack(11, items))
+        self.assertEqual((40, {18, 22}), knapsack(11, items))
 
