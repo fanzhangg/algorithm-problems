77dd863a0b4f1ce61c308c32086afcbaba03ad37
Fan Zhang <vanadiumzhang@gmail.com>
2019-04-23 14:33:46 -0500

    CHANGE: weighted interval scheduling - ADD: a value table

--- dynamic_programming/weighted_interval_scheduling/test_recursive_wis.py
+++ dynamic_programming/weighted_interval_scheduling/test_recursive_wis.py
@@ -20,3 +20,8 @@ class TestWeightedIntervalScheduling(TestCase):
         jobs = [Job(1, 4, 5), Job(3, 5, 3), Job(0, 6, 4), Job(4, 7, 4), Job(3, 8, 5),
                 Job(5, 9, 7), Job(6, 10, 5), Job(8, 11, 7)]
         self.assertEqual((16, [8, 4, 1]), wis(jobs))
+        # j   | 0  1  2  3  4  5  6  7  8
+        # ------------------------------
+        # P   | /  /  /  /  0  /  1  2  4
+        # OPT | 0  5  5  5  9  9 12 12 16
+        # job | /  5  3  4  4  5  7  5  7
