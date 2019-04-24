6ebeb2b018b29ba50798ecad6950ac7c47bc3662
Fan Zhang <vanadiumzhang@gmail.com>
2019-04-24 16:44:59 -0500

    CHANGE: knapsack - return name of item instead of its value

--- dynamic_programming/knapsack/knapsack.py
+++ dynamic_programming/knapsack/knapsack.py
@@ -2,16 +2,20 @@ from typing import List, Tuple
 
 
 class Item(object):
-    def __init__(self, value: int, weight: int):
+    def __init__(self, name: str, value: int, weight: int):
+        self.name = name
         self.value = value
         self.weight = weight
+
+    def __str__(self):
+        return self.name
 
 
 def knapsack(max_weight: int, items: List[Item])->Tuple[int, set]:
     """
     :param max_weight: the capacity of knapsack (W > 0)
-    :param items: n items, each item has a weight > 0 and a value >0
-    :return: a set of items, such as the total weights <= W, and the total value is maximized
+    :param items: n items, each item has a weight > 0 and a name >0
+    :return: a set of items, such as the total weights <= W, and the total name is maximized
     """
     # a matrix opt[items size(0,...,n)[weight(0, ..., total_weight]]
     opt = [[-1 for _ in range(max_weight + 1)] for _ in range(len(items) + 1)]
@@ -32,7 +36,7 @@ def knapsack(max_weight: int, items: List[Item])->Tuple[int, set]:
                 else:
                     opt[i][w] = items[i - 1].value + opt[i - 1][w - items[i - 1].weight]
                     item_set = item_sets[i - 1][w - items[i - 1].weight].copy()
-                    item_set.add(items[i - 1].value)
+                    item_set.add(items[i - 1].name)
                     item_sets[i][w] = item_set
     return opt[-1][-1], item_sets[-1][-1]
 
--- dynamic_programming/knapsack/test_knapsack.py
+++ dynamic_programming/knapsack/test_knapsack.py
@@ -1,9 +1,28 @@
 from unittest import TestCase
 from knapsack import knapsack, Item
+import csv
 
 
 class TestKnapsack(TestCase):
+    @staticmethod
+    def get_items() -> list:
+        items = []
+        with open("knapsackItems.csv") as file:
+            reader = csv.reader(file)
+            line_num = 0
+            for row in reader:
+                if line_num == 0:
+                    pass
+                else:
+                    name = row[0]
+                    value = int(row[1])
+                    weight = int(row[2])
+                    items.append(Item(name, value, weight))
+                line_num += 1
+        return items
+
     def test_knapsack(self):
-        items = [Item(1, 1), Item(6, 2), Item(18, 5), Item(22, 6), Item(28, 7)]
-        self.assertEqual((40, {18, 22}), knapsack(11, items))
+        items = self.get_items()
+        print(knapsack(11, items))
+        self.assertEqual((40, {'item4', 'item3'}), knapsack(11, items))
 
