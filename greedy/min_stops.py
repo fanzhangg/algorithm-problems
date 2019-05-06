d3d0eddcf6077801826a571291ec63715c8d89f1
Fan Zhang <vanadiumzhang@gmail.com>
2019-05-06 18:34:21 -0500

    CHANGE: return the correct indices of stops

--- greedy/min_stops.py
+++ greedy/min_stops.py
@@ -3,17 +3,15 @@ from typing import List
 
 def min_stops(dists: List[int], max_walk: int):
     """
-    :param dists: a list of distances between the current stop and the former stop
-    :param max_walk: the person can travel at most x miles per time
-    :return: the stops minimize the number of stops
+    :param dists: a list of distances between the current stop and the former stop, let the start stop be stop 0
+    :param max_walk: miles per time of the maximum traveling distance
+    :return: the indices of stops minimize the number of stops
     :idea: choose stop i such as maximize valid (dists[i] - dists[i-1]); a stop i is valid if d[i] - d[i-1] <= max_walk
     """
     selected_stops = []
-    # do we assume a valid solution always exist?
-    # is stop n Kholinar?
     walk_dist = 0
 
-    for i in range(0, len(dists)):
+    for i in range(len(dists)):
         if dists[i] > max_walk:
             return None
             # the former stop is the valid stop max(dists[i] - dists[i -1])
@@ -21,6 +19,7 @@ def min_stops(dists: List[int], max_walk: int):
             walk_dist += dists[i]
 
         if walk_dist > max_walk:
-            selected_stops.append(i - 1)
+            print(dists[i])
+            selected_stops.append(i)
             walk_dist = dists[i]
     return selected_stops
--- greedy/test_min_stops.py
+++ greedy/test_min_stops.py
@@ -5,7 +5,7 @@ from min_stops import *
 class TestMinStops(TestCase):
     def test_valid_stops(self):
         dists = [1, 3, 10, 3, 5, 4, 10, 2, 8, 9]
-        self.assertEqual([3, 6], min_stops(dists, 20))
+        self.assertEqual([4, 7], min_stops(dists, 20))
 
     def test_invalid_stops(self):
         dists = [6, 2, 2, 2, 2, 2, 2, 2]
