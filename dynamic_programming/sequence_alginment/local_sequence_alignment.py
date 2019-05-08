111de1d567ac2b3d2ea08949e38f6a9894989175
Fan Zhang <vanadiumzhang@gmail.com>
2019-05-07 22:18:27 -0500

    CHANGE: get the optimal score CHANGE: traceback to find the optimal alignment

--- dynamic_programming/sequence_alginment/local_sequence_alignment.py
+++ dynamic_programming/sequence_alginment/local_sequence_alignment.py
@@ -14,7 +14,7 @@ def local_alignment(str1: str, str2: str, gap_cost: int):
     """
     :param str1: a string longer than str2
     :param str2:
-    :param gap_cost:
+    :param gap_cost: a negative int
     :return:
     """
     # create a (n+1) * (m+1) matrix
@@ -28,13 +28,67 @@ def local_alignment(str1: str, str2: str, gap_cost: int):
     for i in range(1, len(str1) + 1):
         for j in range(1, len(str2) + 1):
             # if align str1[i-1] and str2[j-1]
-            opt1 = opt[i - 1][j - 1] + similarity(str1[i - 1], str2[j - 1])
+            match = opt[i - 1][j - 1] + similarity(str1[i - 1], str2[j - 1])
             # if str1[i-1] is at the end of a gap of a gap of length k
-            opt2 = opt[i - 1][j] - gap_cost
+            delete = opt[i - 1][j] + gap_cost
             # if str2[j-1] is at the end of a gap of length l
-            opt3 = opt[i][j - 1] - gap_cost
-            opt4 = 0
-            opt[i][j] = max(opt1, opt2, opt3, opt4)
-    for line in opt:
-        print(line)
-    return opt[-1][-1]
+            insert = opt[i][j - 1] + gap_cost
+            opt[i][j] = max(match, delete, insert, 0)
+
+
+    def get_optimal_score():
+        """
+        :return: the score of the optimal solution such as the highest score in the matrix
+        """
+        max_score = 0
+        curr_i = 0
+        curr_j = 0
+        for i in range(len(str1) + 1):
+            for j in range(len(str2) + 1):
+                if opt[i][j] > max_score:
+                    max_score = opt[i][j]
+                    curr_i, curr_j = i, j
+        return max_score, curr_i, curr_j
+
+    align_1_list = []
+    align_2_list = []
+
+    def trace_back(i, j):
+        # if i == j == 1:
+        #     # convert the list to str
+        #     pass
+        # else:
+        char1 = str1[i - 1]
+        char2 = str2[j - 1]
+
+        # if align str1[i-1] and str2[j-1]
+        match = opt[i - 1][j - 1] + similarity(str1[i - 1], str2[j - 1])
+        # if str1[i-1] is at the end of a gap of a gap of length k
+        delete = opt[i - 1][j] + gap_cost
+        # if str2[j-1] is at the end of a gap of length l
+        insert = opt[i][j - 1] + gap_cost
+        opt4 = 0
+        max_opt = max(match, delete, insert, opt4)
+
+        if match == max_opt:
+            # insert both letters to the string
+            align_1_list.insert(0, char1)
+            align_2_list.insert(0, char2)
+            trace_back(i-1, j-1)
+        elif delete == max_opt:
+            align_1_list.insert(0, char1)
+            align_2_list.insert(0, "-")
+            trace_back(i-1, j)
+        elif insert == max_opt:
+            align_1_list.insert(0, "-")
+            align_2_list.insert(0, char2)
+            trace_back(i, j-1)
+        elif max_opt == 0:
+            pass
+
+    optimal_score, opt_i, opt_j = get_optimal_score()
+    trace_back(opt_i, opt_j)
+
+    optimal_aligns = "".join(align_1_list), "".join(align_2_list)
+
+    return optimal_score, optimal_aligns
--- dynamic_programming/sequence_alginment/test_local_alignment.py
+++ dynamic_programming/sequence_alginment/test_local_alignment.py
@@ -6,5 +6,4 @@ class TestLocalAlignment(TestCase):
     def test_local_alignment(self):
         s2 = "TGTTACGG"
         s1 = "GGTTGACTA"
-        max_score = local_alignment(s1, s2, 2)
-        print(max_score)
+        self.assertEqual((13, ('GTTGAC', 'GTT-AC')), local_alignment(s1, s2, -2))
