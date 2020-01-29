c85218fcf6d0cca043cb52a9c2283f4d512f8b7a
fanzhangg <vanadiumzhang@gmail.com>
2020-01-28 23:17:50 -0600

    ADD: horner

--- math/linear/functions.py
+++ math/linear/functions.py
@@ -1,3 +1,22 @@
+def horner(coeffs: [int or float], x: int or float, is_decr=True)->int:
+    """
+    This function evaluate a polynomial via Horner's method
+    :param coeffs:
+    :param x:
+    :param is_decr:
+    :return:
+    """
+    if is_decr:
+        ans = coeffs[0]
+        for i in range(1, len(coeffs)):
+            ans = ans * x + coeffs[i]
+    else:
+        ans = coeffs[-1]
+        for i in range(len(coeffs) - 2, -1, -1):
+            ans = ans * x + coeffs[i]
+    return ans
+
+
 def nest(coeefs: list, x: int or float, base_points: list)->int or float:
     """
     Evaluates polynomial from nested form using Horner’s Method
@@ -12,5 +31,5 @@ def nest(coeefs: list, x: int or float, base_points: list)->int or float:
     return y
 
 
-ans = nest([1, 1/2, 1/2, -1/2], 1, [0, 2, 3])
-print(ans)
+nest([1, 1/2, 1/2, -1/2], 1, [0, 2, 3])
+horner([13, 9, 2, 6, 3], 2, is_decr=False)
