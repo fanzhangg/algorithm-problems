4ded0e16e87f8d33b35d947032d53d3d43980962
fanzhangg <vanadiumzhang@gmail.com>
2020-02-01 15:12:21 -0600

    ADD: binary euler tour

--- trees/applications/euler_tour.py
+++ trees/applications/euler_tour.py
@@ -38,14 +38,14 @@ class EulerTour:
             results.append(self._tour(c, dep + 1, path))    # Recursion on the children's subtree
             path[-1] += 1   # Increment index
         path.pop()
-        ans = self._hook_postvisit(pos, dep, results)  # post visit p
+        ans = self._hook_postvisit(pos, dep, path, results)  # post visit p
         return ans
 
     def _hook_previsit(self, pos: Tree.Position, dep: int, path: [int]):
         """Can be overridden"""
         pass
 
-    def _hook_postvisit(self, pos: Tree.Position, dep: int, path: [int]):
+    def _hook_postvisit(self, pos: Tree.Position, dep: int, path: [int], results: [int]):
         """Can be overridden"""
         pass
 
@@ -63,6 +63,46 @@ class ParenthesizeTour(EulerTour):
         if not self.tree().is_leaf(pos):
             print(" (", end="")
 
-    def _hook_postvisit(self, pos: Tree.Position, dep: int, path: [int]):
+    def _hook_postvisit(self, pos: Tree.Position, dep: int, path: [int], results: [int]):
         if not self.tree().is_leaf(pos):
             print(")", end="")
+
+
+class BinaryEulerTour(EulerTour):
+    """
+    Abstract base class for performing Euler tour of a binary tree
+    """
+    def _tour(self, pos: Tree.Position, dep: int, path: [int]):
+        results = [None, None]
+        self._hook_previsit(pos, dep, path)
+        if self._tree.left(pos) is not None:    # Left child
+            path.append(0)
+            results[0] = self._tour(self._tree.left(pos), dep + 1, path)
+            path.pop()
+        self._hook_invisit(pos, dep, path)
+        if self._tree.right(pos) is not None:   # Right child
+            path.append(1)
+            results[1] = self._tour(self._tree.right(pos), dep + 1, path)
+            path.pop()
+        ans = self._hook_postvisit(pos, dep, path, results)     # Post visit
+        return ans
+
+    def _hook_invisit(self, pos: Tree.Position, dep: int, path: [int]):
+        """
+        Can be overriden
+        """
+        pass
+
+
+class BinaryLayout(BinaryEulerTour):
+    """
+    Class for computing (x, y) coordinates for each of a binary tree
+    """
+    def __init__(self, tree: Tree):
+        super().__init__(tree)
+        self._count = 0
+
+    def _hook_invisit(self, pos: Tree.Position, dep: int, path: [int]):
+        pos.element().setX(self._count)
+        pos.element().setY(dep)
+        self._count += 1
