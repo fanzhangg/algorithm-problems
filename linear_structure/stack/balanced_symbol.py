deeb22b6101991738d37af13cf1ee2219d04f2f0
fanzhangg <vanadiumzhang@gmail.com>
2020-03-17 12:05:54 -0500

    Add some comments to balanced_symbol

--- linear_structure/stack/balanced_symbol.py
+++ linear_structure/stack/balanced_symbol.py
@@ -1,29 +1,34 @@
-"""
-Given: A string of mixed symbols
-Output: True if each maintains its own opeen and close relationships; False if not
-"""
-from stack.stack import Stack
+from ds.stack import ArrayStack
 
 
 def is_balanced_symbols(string: str):
-    stack = Stack()
+    """
+    :param string: A string of mixed symbols
+    :return True if each bracket maintains its own open and close relationships; False if not
+    """
+    stack = ArrayStack()
     for char in string:
         if char in "([{":
             stack.push(char)
         elif char in "}])":
-            if stack.isempty():
+            if stack.is_empty():
                 return False
-            elif match(stack.peek(), char):
+            elif match(stack.top(), char):
                 stack.pop()
             else:
                 return False
-    if stack.isempty():
+    if stack.is_empty():
         return True
     else:
         return False
 
 
 def match(opener: str, closer: str) -> bool:
+    """
+    :param opener: a character in "([{"
+    :param closer: a character in ")]}"
+    :return: True if the opener matches the closer
+    """
     openers = "([{"
     closers = ")]}"
     return openers.index(opener) == closers.index(closer)
